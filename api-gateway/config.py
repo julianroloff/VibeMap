832cf24c5b95473c4cf8927e4723b19bce771a11
Julian Roloff <jroloff2@smail.uni-koeln.de>
2025-02-13 19:07:11 +0100

    Basic Folder Structure

---
+++ api-gateway/config.py
@@ -0,0 +1,9 @@
+# Configuration Settings
+
+SERVICES = {
+    "auth_service": "http://localhost:8001",
+    "location_service": "http://localhost:8002",
+    "heatmap_service": "http://localhost:8003",
+    "ml_service": "http://localhost:8004",
+    "external_api_fetcher": "http://localhost:8005"
+}
---
+++ api-gateway/main.py
@@ -0,0 +1,17 @@
+# Routes requests to microservices
+
+from fastapi import FastAPI
+from routes import router
+
+app = FastAPI(title="API Gateway")
+
+
+# router for redirecting API calls
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port= 8000)
+
+
+    
