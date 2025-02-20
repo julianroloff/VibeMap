3d7d726b62e573aacceef0f5c86ecb4ed268624c
Ágoston Reischl <58388975+reischlago@users.noreply.github.com>
2025-02-20 20:23:08 +0100

    settings, help and feedback placeholder pages

--- frontend/quasar-project/src/pages/HelpPage.vue
+++ frontend/quasar-project/src/pages/HelpPage.vue
@@ -5,8 +5,12 @@
         Help page
       </div>
 
-      <div class="text-h6" style="opacity:.4">
-        Oops. Nothing here yet...
+      <div class="text-h6 text-left">
+        <p>Use the Map to locate stress-free zones around you! </p>
+        <p>After logging in, you can also leave ratings of your surrounding, and let other know if a spot is stressful or not.</p>
+        <p>You can find your previous ratings in the 'My ratings' page of the application that you can access from the left side panel.</p>
+        <p>If you would like to leave a feedback about the application, please visit the 'Feedback' page to see your options.</p>
+        <p>Most importantly: enjoy Amsterdam stress-free!</p>
       </div>
 
       <q-btn
